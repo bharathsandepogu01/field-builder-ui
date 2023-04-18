8742cc4db304438995557a9a6f46015e2488802f
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-18 16:10:37 +0530

    initial-commit: field builder ui project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
